4e39574ffae6c4f066c2a67481cae5049c3be5af
bachlaoto <bachlaoto@gmail.com>
2018-10-08 18:14:00 +0700

    angular

--- angular6/src/app/app.module.ts
+++ angular6/src/app/app.module.ts
@@ -9,9 +9,15 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {HeaderComponent} from './header/header.component';
 import {ClubService} from './service/club.service';
-import { ListClubComponent } from './club/list-club/list-club.component';
-import { AddClubComponent } from './club/add-club/add-club.component';
-import { EditClubComponent } from './club/edit-club/edit-club.component';
+import {ListClubComponent} from './club/list-club/list-club.component';
+import {AddClubComponent} from './club/add-club/add-club.component';
+import {EditClubComponent} from './club/edit-club/edit-club.component';
+import {ListSponsorComponent} from './sponsor/list-sponsor/list-sponsor.component';
+import {AddSponsorComponent} from './sponsor/add-sponsor/add-sponsor.component';
+import {EditSponsorComponent} from './sponsor/edit-sponsor/edit-sponsor.component';
+import {ListPlayerComponent} from './player/list-player/list-player.component';
+import {AddPlayerComponent} from './player/add-player/add-player.component';
+import {EditPlayerComponent} from './player/edit-player/edit-player.component';
 
 
 @NgModule({
@@ -21,7 +27,13 @@ import { EditClubComponent } from './club/edit-club/edit-club.component';
     HeaderComponent,
     ListClubComponent,
     AddClubComponent,
-    EditClubComponent
+    EditClubComponent,
+    ListSponsorComponent,
+    AddSponsorComponent,
+    EditSponsorComponent,
+    ListPlayerComponent,
+    AddPlayerComponent,
+    EditPlayerComponent
   ],
   imports: [
     BrowserModule,
